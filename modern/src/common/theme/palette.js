5375bfde98ea1d14cc4643497a52947482d77fc3
DinithHerath <dinithherath18332@gmail.com>
2022-11-15 00:17:24 +0530

    Make theme customisable

--- modern/src/common/theme/palette.js
+++ modern/src/common/theme/palette.js
@@ -1,17 +1,18 @@
 import {
-  amber, grey, green, indigo, red, common,
+  amber, grey, green, red, indigo, common,
 } from '@mui/material/colors';
+import themeConfigurations from '../../config/theme';
 
 const colors = {
   white: common.white,
-  background: grey[50],
-  primary: indigo[900],
-  secondary: green[500],
-  positive: green[500],
-  medium: amber[700],
-  negative: red[500],
-  neutral: grey[500],
-  geometry: '#3bb2d0',
+  background: themeConfigurations?.background ?? grey[50],
+  primary: themeConfigurations?.primary ?? indigo[900],
+  secondary: themeConfigurations?.secondary ?? green[500],
+  positive: themeConfigurations?.positive ?? green[500],
+  medium: themeConfigurations?.medium ?? amber[700],
+  negative: themeConfigurations?.negative ?? red[500],
+  neutral: themeConfigurations?.neutral ?? grey[500],
+  geometry: themeConfigurations?.geometry ?? '#3bb2d0',
 };
 
 export default {
---
+++ modern/src/config/theme.js
@@ -0,0 +1,16 @@
+const themeConfigurations = {
+  /*
+    This file can be used to override the default theme configurations in common/theme/palette.js
+    Uuncomment the configuration to override the default theme configurations
+  */
+  // background: grey[50],
+  // positive: green[500],
+  // medium: amber[700],
+  // negative: red[500],
+  // neutral: grey[500],
+  // geometry: '#3bb2d0',
+  // secondary: '#3aaa34',
+  primary: '#cd1201',
+};
+
+export default themeConfigurations;
