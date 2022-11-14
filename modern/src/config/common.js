12293cd92f167f008817e2fff9a91a287e16703a
DinithHerath <dinithherath18332@gmail.com>
2022-11-15 00:17:50 +0530

    Add configuration file

---
+++ modern/src/config/common.js
@@ -0,0 +1,10 @@
+const commonConfigurations = {
+  /*
+  This file can be used to override the default configurations
+  Uncomment the configuration to override the default configurations
+  */
+  customLogo: false, // true to use custom logo defined in config/images/custom.svg
+  font: 'Rubik', // font family to be used in the application. add the google font link in style.css
+};
+
+export default commonConfigurations;
