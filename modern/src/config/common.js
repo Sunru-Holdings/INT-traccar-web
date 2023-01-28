e7584d8b4539e2ed808c7729debb0e8e2fb83fed
DinithHerath <dinithherath18332@gmail.com>
2023-01-28 20:36:41 +0530

    Add Sunru custom logo

--- modern/src/config/common.js
+++ modern/src/config/common.js
@@ -3,7 +3,7 @@ const commonConfigurations = {
   This file can be used to override the default configurations
   Uncomment the configuration to override the default configurations
   */
-  customLogo: false, // true to use custom logo defined in config/images/custom.svg
+  customLogo: true, // true to use custom logo defined in config/images/custom.svg
   font: 'Rubik', // font family to be used in the application. add the google font link in style.css
 };
 
