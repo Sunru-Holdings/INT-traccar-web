c8f75c26577d318b9a3612eea3f2f252265cf054
Dinith Herath <dinithherath18332@gmail.com>
2024-04-09 00:17:23 +0530

    Change version number

--- src/common/theme/index.js
+++ src/common/theme/index.js
@@ -3,7 +3,6 @@ import { createTheme } from '@mui/material/styles';
 import palette from './palette';
 import dimensions from './dimensions';
 import components from './components';
-import typography from './typography';
 
 export default (server, darkMode) => useMemo(() => createTheme({
   palette: palette(server, darkMode),
